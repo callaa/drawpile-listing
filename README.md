52aa2d1b68cf4b2b2f90594f31e69afaa352d595
Calle Laakkonen <laakkonenc@gmail.com>
2015-06-21 18:13:57 +0300

    Add CherryPy implementation (initial)

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 Drawpile Public Listing Server
 ------------------------------
 
-This is a sample implementation of a Drawpile public listing server.
-At the moment, just a PHP implementation is provided.
+This is a collection of sample implementations of a Drawpile public listing server.
 
-To install, first create the database. The included creation script is for
-MySQL/MariaDB, but the the server itself should run just as well on PostgreSQL too.
+To install, first create the database. Scripts are provided for MySQL/MariaDB and Postgresql.
+The PHP based server uses MySQL and the Python based on Postgresql.
+
+To use your server, go to Drawpile's preferences dialog, click on
+the *Add* button (in the *List servers* tab) and enter the URL to your listing server. (E.g. `http://example.com/my-listing-server/`)
+
+Note. The list server scripts will never delete anything from the database, so
+you should periodically run the `cleanup-database.sql` to keep old listings
+from accumulating.
+
+## Installing the PHP based server
 
 Copy the contents of `php/` to your server. Remember to copy the `.htaccess` file as well!
 
 Rename or copy `config.php.sample` to `config.php` and modify the the settings
 to suit your server environment. You should now have a functioning Drawpile listing server!
 
-To use it, go to Drawpile's preferences dialog, click on
-the *Add* button (in the *List servers* tab) and enter the URL to your listing server. (E.g. `http://example.com/my-listing-server/`)
+## Installing the Python (CherryPy) based server
 
-Note. The list server script will never delete anything from the database, so
-you should periodically run the `cleanup-database.sql` to keep old listings
-from accumulating.
+1. (Optional) Create a virtualenv
+2. Install the dependencies with `pip install -r requirements.txt`
+3. Copy `settings.py.sample` to `settings.py` and adjust to suit your environment.
+4. Configure nginx/apache/other web server to proxy traffic to the application. (See various CherryPy deployment guides)
+
---
+++ cherrypy/sessionapi.py
@@ -0,0 +1,238 @@
+import cherrypy
+import random
+import string
+import settings
+import re
+import json
+import datetime
+
+SESSION_ID_RE = re.compile(r'^[a-zA-Z0-9:-]{1,64}$')
+
+
+class SessionListingApi(object):
+
+    """API root object"""
+    exposed = True
+
+    def __init__(self):
+        self.sessions = Sessions()
+
+    def GET(self):
+        return {
+            'api_name': 'drawpile-session-list',
+            'version': '1.0',
+            'name': settings.NAME,
+            'description': settings.DESCRIPTION,
+            'favicon': settings.FAVICON,
+        }
+
+
+class Sessions(object):
+
+    """Session listing API"""
+    exposed = True
+
+    def GET(self):
+        """Get list of active sessions"""
+
+        sql = '''
+			SELECT host, port, session_id as id, protocol, owner, title, users, password, started
+			FROM drawpile_sessions
+			WHERE unlisted=false AND last_active >= current_timestamp - interval %s'''
+        params = [settings.SESSION_TIMEOUT]
+
+        with settings.db() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, params)
+
+                columns = tuple(x[0] for x in cur.description)
+                sessions = [Session(**dict(zip(columns, row))) for row in cur]
+
+        return [s.to_json() for s in sessions]
+
+    def POST(self):
+        """Announce a new session"""
+        data = cherrypy.request.json
+
+        pk, updatekey = Session.create(data, cherrypy.request.remote.ip)
+
+        return {
+            'id': pk,
+            'key': updatekey,
+        }
+
+    def _cp_dispatch(self, vpath):
+        if len(vpath) == 1 and vpath[0].isdigit():
+            session = Session(pk=int(vpath[0]))
+            vpath.pop(0)
+            return session
+
+        return None
+
+
+class Session(object):
+
+    """API for updating individual sessions"""
+
+    exposed = True
+
+    # Public attributes
+    ATTRS = ('host', 'port', 'id', 'protocol', 'title',
+             'users', 'password', 'owner', 'started')
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.iteritems():
+            setattr(self, k, v)
+
+    def PUT(self):
+        """Refresh this announcement"""
+        data = cherrypy.request.json
+        with settings.db() as conn:
+            with conn.cursor() as cur:
+                self._check_update_key(cur)
+
+                set_sql = ['last_active = current_timestamp']
+                params = []
+                if data.get('title', None):
+                    set_sql.append('title=%s')
+                    params.append(data['title'])
+
+                if data.get('users', None):
+                    set_sql.append('users=%s')
+                    params.append(data['users'])
+
+                if data.get('password', None):
+                    set_sql.append('password=%s')
+                    params.append(data['password'])
+
+                if data.get('owner', None):
+                    set_sql.append('owner=%s')
+                    params.append(data['owner'])
+
+                sql = 'UPDATE drawpile_sessions SET ' +\
+                    ', '.join(set_sql) +\
+                    ' WHERE id=%s'
+                params.append(self.pk)
+
+                cur.execute(sql, params)
+
+                return {'status': 'ok'}
+
+    def DELETE(self):
+        """Unlist this announcement"""
+        with settings.db() as conn:
+            with conn.cursor() as cur:
+                self._check_update_key(cur)
+
+                cur.execute(
+                    'UPDATE drawpile_sessions SET unlisted=true WHERE id=%s', [self.pk])
+
+                cherrypy.response.status = 204
+
+    def _check_update_key(self, cursor):
+        """Check if the given update key is valid for the listing"""
+        cur.execute(
+            """SELECT update_key FROM drawpile_sessions
+			WHERE id=%s
+			AND unlisted=false
+			AND last_active >= current_timestamp - interval %s""",
+            [self.pk, settings.SESSION_TIMEOUT]
+        )
+        row = cur.fetchone()
+        if not row:
+            raise cherrypy.HTTPError(404)
+        elif row[0] != cherrypy.request.headers.get('HTTP_X_UPDATE_JEY'):
+            raise cherrypy.HTTPError(403)
+
+    @classmethod
+    def create(cls, data, client_ip):
+        # Validate data
+        if not SESSION_ID_RE.match(data['id']):
+            raise cherrypy.HTTPError(422, "BADDATA:Invalid ID")
+
+        host = data.get('host', client_ip)
+
+        # TODO validate host name
+
+        if 'port' not in data:
+            port = 27750
+        else:
+            if not data['port'].isdigit():
+                raise cherrypy.HTTPError(422, "BADDATA:Invalid port number")
+            port = int(data['port'])
+            if port <= 0 or port >= 65536:
+                raise cherrypy.HTTPError(422, "BADDATA:Invalid port number")
+
+        sql = '''INSERT INTO drawpile_sessions
+			(host, port, session_id, protocol, owner, title, users, password, update_key, client_ip) 
+			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+
+        with settings.db() as conn:
+            with conn.cursor() as cur:
+                # Rate limiting
+                cur.execute('''SELECT COUNT(id) FROM drawpile_sessions
+					WHERE client_ip=%s AND last_active >= current_timestamp - interval %s''',
+                            [client_ip, settings.SESSION_TIMEOUT]
+                            )
+                count = cur.fetchone()[0]
+                if count > settings.RATELIMIT:
+                    raise cherrypy.HTTPError(
+                        429, "You have announced too many sessions ({0}) too quickly!".format(count))
+
+                # Check for duplicates
+                cur.execute('''SELECT COUNT(id) FROM drawpile_sessions
+					WHERE host=%s AND port=%s AND session_id=%s AND unlisted=false AND last_active >= current_timestamp - interval %s''',
+                            [host, port, data['id'], settings.SESSION_TIMEOUT]
+                            )
+                count = cur.fetchone()[0]
+                if count > 0:
+                    raise cherrypy.HTTPError(
+                        422, "DUPLICATE:session already listed")
+
+                # OK: insert entry
+                update_key = ''.join(random.SystemRandom().choice(
+                    string.ascii_uppercase + string.digits) for _ in range(16))
+                try:
+                    cur.execute(sql, (
+                        host,
+                        port,
+                        data['id'],
+                        data['protocol'],
+                        data['owner'],
+                        data.get('title', ''),
+                        data['users'],
+                        data.get('password', False),
+                        update_key,
+                        client_ip
+                    ))
+                except KeyError as ke:
+                    raise cherrypy.HTTPError(422, "BADDATA:" + str(ke))
+
+                pk = cur.lastrowid
+
+                return (pk, update_key)
+
+    def to_json(self):
+        json = {a: getattr(self, a) for a in self.ATTRS}
+        json['started'] = unicode(json['started'])
+        return json
+
+
+def error422(status, message, traceback, version):
+    code, msg = message.split(':', 1)
+    return json.dumps({
+        'error': code,
+        'message': msg
+    })
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.json_in.on': True,
+            'tools.json_out.on': True,
+            'error_page.422': error422,
+            'request.show_tracebacks': False,
+        }
+    }
+    cherrypy.quickstart(SessionListingApi(), '/', conf)
